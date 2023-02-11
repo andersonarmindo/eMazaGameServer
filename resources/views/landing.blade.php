64c668ba9fa3a8380a2f5bb839e7858a7a0e7057
Anderson Armindo João <andersonarmindojoao@outlook.com>
2023-02-11 17:35:05 +0200

    Upload do template

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-pt" class="no-js">
+<head>
+@include('base.include-seo')
+@include('base.include-css')
+</head>
+<body class="is-boxed has-animations">
+<div class="body-wrap">
+@include('base.landing.header')
+    @include('base.landing.main')
+    @include('base.landing.footer')
+
+
+</div>
+
+@include('base.include-js')
+</body>
+</html>
